1ec2ff9d7b945a99cebd661fb635315b43bbf89a
Peter Chernoff <peter.chernoff@gmail.com>
2016-08-19 08:15:49 -0400

    Using strings rather than pandas timestamps for the last part of the calculations

--- BikeProject/bikeaccidents/bikeaccidents.py
+++ BikeProject/bikeaccidents/bikeaccidents.py
@@ -38,13 +38,14 @@ class BikeAccidents:
         # to_datetime is to get the times to a more usable format
         data.loc[:, 'Time'] = pd.to_datetime(data['Time'])
         data.loc[:, 'Date'] = pd.to_datetime(data['Date'])
-        
-        min_time = "00:00:00" if not start_time else start_time
-        max_time = "23:59:59" if not start_time else end_time
-        
+            
+        # We want the string value of our dates, which are the last values depicted
+        min_time = "00:00:00" if not start_time else str(start_time)[-8:]
+        max_time = "23:59:59" if not start_time else str(end_time)[-8:]
+                
         min_date = data["Date"].min() if not start_date else start_date
         max_date = data["Date"].max() if not end_date else end_date
-        
+
         # We might want to cover a range late at night from 23:00 to 1:00, so we want to have the option to wrap around
         if min_time <= max_time:
             # The more intuitive, is the value between the lower value and the higher value.
@@ -69,9 +70,9 @@ class BikeAccidents:
         else:
             data = data[(data['Date'] >= pd.to_datetime(min_date)) |
                         (data['Date'] <= pd.to_datetime(max_date))]
-        
+
         return data
-    
+
     def accident_count(self, data_to_map):
         # amalgamates the values that happen at a cerain location
         accident_count_location = data_to_map['Geocoding address'].value_counts()
